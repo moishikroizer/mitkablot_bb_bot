9ca75214f3bbb4daf33de1594f88c2e878cf80fa
moishikroizer <m0533125104@gmail.com>
2025-08-05 03:02:05 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import os
 from telegram import Update
 from telegram.ext import ApplicationBuilder, ContextTypes, MessageHandler, filters
 
-# מזהים מהסביבה
 BOTLOG_CHANNEL = '@botlog_1221'
 ADMIN_USERNAME = '@moishi_5104'
 
@@ -10,30 +9,20 @@ async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
     user = update.effective_user
     message = update.message.text or "<הודעה ללא טקסט>"
 
-    # פרסום בערוץ
     try:
-        channel_text = "📥 הודעה חדשה שהתקבלה:
-
-{}".format(message)
-        await context.bot.send_message(
-            chat_id=BOTLOG_CHANNEL,
-            text=channel_text
-        )
+        text_to_channel = "התקבלה הודעה חדשה:\n\n" + message
+        await context.bot.send_message(chat_id=BOTLOG_CHANNEL, text=text_to_channel)
     except Exception as e:
-        print("שגיאה בשליחת הודעה לערוץ:", e)
+        print("שגיאה בשליחה לערוץ:", e)
 
-    # שליחת שם המשתמש למנהל
     try:
-        username = user.username or f"ID:{user.id}"
-        admin_text = "התקבלה הודעה מ: @{}".format(username)
-        await context.bot.send_message(
-            chat_id=ADMIN_USERNAME,
-            text=admin_text
-        )
+        username = user.username or ("ID:" + str(user.id))
+        text_to_admin = "התקבלה הודעה מ: @" + username
+        await context.bot.send_message(chat_id=ADMIN_USERNAME, text=text_to_admin)
     except Exception as e:
-        print("שגיאה בשליחת שם משתמש למנהל:", e)
+        print("שגיאה בשליחה למנהל:", e)
 
 if __name__ == '__main__':
-    application = ApplicationBuilder().token(os.getenv("BOT_TOKEN")).build()
-    application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message))
-    application.run_polling()
+    app = ApplicationBuilder().token(os.getenv("BOT_TOKEN")).build()
+    app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message))
+    app.run_polling()
